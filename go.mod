aabcd2b0c074aaab82c2521cf2dd15703a376967
Oscar Andersson <oscar.andersson.200@gmail.com>
2024-11-26 19:29:02 +0100

    Adds cron and config

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module oscar230/billetto-discord-webhook
 
 go 1.19
+
+require (
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/robfig/cron/v3"
+	"gopkg.in/yaml.v3"
+)
+
+type Webhooks struct {
+	WebhookUrl string   `yaml:"url"`
+	Events     []string `yaml:"events"`
+	Enabled    bool     `yaml:"enabled"`
+	Silent     bool     `yaml:"silent"`
+}
+
+type Config struct {
+	DatabaseUrl    string     `yaml:"database"`
+	CronExpression string     `yaml:"interval"`
+	Webhooks       []Webhooks `yaml:"webhooks"`
+}
+
+func loadConfig() Config {
+	// Open the config file
+	file, err := os.Open("config.yaml")
+	if err != nil {
+		log.Fatal("Error opening config file:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	// Decode the YAML file into the Config struct
+	var config Config
+	decoder := yaml.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Fatal("Error decoding config file:", err)
+		os.Exit(1)
+	}
+	return config
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	config := loadConfig()
+	log.Print("Loaded Configuration.")
+
+	// Create a new cron scheduler
+	scheduler := cron.New()
+
+	// Add a task with a cron expression
+	scheduler.AddFunc(config.CronExpression, func() {
+		fmt.Println("Task running at", time.Now())
+	})
+
+	// Start the cron scheduler
+	scheduler.Start()
+	log.Print("Scheduler started.")
+
+	// Keep the program running to observe scheduled tasks
+	select {}
 }
